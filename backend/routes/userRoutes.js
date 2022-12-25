6a562e9333403b0cd12d85de7918037cc534563d
Eddie Abramov <ediabc11@gmail.com>
2022-12-25 17:31:20 +0200

    add signup screen

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -26,4 +26,23 @@ userRouter.post(
     })
 );
 
+userRouter.post(
+    '/signup',
+    expressAsyncHandler(async (req, res) => {
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password),
+            isAdmin: false
+        });
+        const user = await newUser.save();
+        res.send({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user),
+            isAdmin: user.isAdmin
+        });
+    })
+);
 export default userRouter;
