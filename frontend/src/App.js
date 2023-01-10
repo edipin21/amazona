f20c6eba649e58f717cc3c661f01fbe5bec850f5
Eddie Abramov <ediabc11@gmail.com>
2023-01-10 14:19:07 +0200

    add orders list screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,6 +30,7 @@ import DashboardScreen from './screens/DashboardScreen';
 import AdminRoute from './components/AdminRoute';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
+import OrderListScreen from './screens/OrderListScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -187,6 +188,11 @@ function App() {
               <Route path='/admin/products'
                 element={<AdminRoute>
                   <ProductListScreen />
+                </AdminRoute>}>
+              </Route>
+              <Route path='/admin/orders'
+                element={<AdminRoute>
+                  <OrderListScreen />
                 </AdminRoute>}>
               </Route>
               <Route
